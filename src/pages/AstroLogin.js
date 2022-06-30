99df3075c1b111d3ea9da2c4ca78dc54fca22d20
Brian Urban <brian524@revature.net>
2022-06-30 02:12:40 -0500

    Homepage HTML File Deleted

--- src/pages/AstroLogin.js
+++ src/pages/AstroLogin.js
@@ -2,11 +2,40 @@ import * as React from 'react';
 export class AstroLogin extends React.Component {
     constructor(props){
         super(props);
+        this.state = {
+            inputValue : ""
+        }
     }
-render(){
-    return(
-        <div>
-            <form onSubmit= {event=>this.submit(event)}/>
-        </div>
-    )}
+    render(){
+        return(
+            <div>
+                <h1>Login</h1>
+                <input type="text" value = {this.state.inputValue} onInput = {event => this.updateInputValue(event)}></input>
+                    <p>Username</p>
+                    <input type="text" value = {this.state.inputValue} onInput = {event => this.updateInputValue(event)}></input>
+                    <p>Password</p>
+                    <input type="text" value = {this.state.inputValue} onInput = {event => this.updateInputValue(event)}></input> */
+                <button onClick = {event=>this.submit(event)}>submit</button>
+            </div>
+        )
+    }
+    updateInputValue(event){
+        let value = event.target.value;
+        this.setState({
+            inputValue : value
+        })
+    }
+    submit(event){
+        let post = this.state.inputValue;
+        fetch("https://astrolounge.azurewebsites.net/astroUser/login", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": post
+            })
+        });
+    }
 }
